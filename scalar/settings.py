f2156098c4bb4049a01a689e2f587687d1e979f3
305anas305 <anas305akgec@gmail.com>
2024-09-05 19:37:42 +0530

    f

--- scalar/settings.py
+++ scalar/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scalar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scalardb',
+        'USER':'anas503',
+        'PASSWORD':'nizamuddin123',
+        'HOST':'scalardb.cz8w2oci46gh.eu-north-1.rds.amazonaws.com',
+        'PORT':'3306'
     }
 }
 
